fa25d029a0a94df508a3064e7089165ce94457d7
qcprof <anique.qureshi@qc.cuny.edu>
2024-10-07 05:20:45 -0400

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-red-800">Home</h1>
+      <p>Added Changes for github.</p>
       <GetTime />
       <Counter />
     </main>
